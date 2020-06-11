1f88667b1cddccb2947cc942b5a0648abccb1a41
Elisha <elishaubax@gmail.com>
2020-06-11 02:21:00 -0700

    Creating CRUD operations for bills database table

---
+++ server/bills/bills-model.js
@@ -0,0 +1,32 @@
+const db = require("../data/db-config");
+
+module.exports = {
+  find,
+  findById,
+  add,
+  update,
+  remove,
+};
+
+function find() {
+  return db("bills");
+}
+
+function findById(id) {
+  return db("bills").where({ id }).first();
+}
+
+function add(bill) {
+  return db("bills").insert(bill);
+}
+
+function update(id, changes) {
+  return db("bills")
+    .where({ id })
+    .update(changes)
+    .then(() => {
+      return findById(id);
+    });
+}
+
+function remove(id) {}
--- server/bills/bills-router.js
+++ server/bills/bills-router.js
@@ -1,39 +1,70 @@
 const express = require("express");
 
+const db = require("../data/db-config.js");
+const Bills = require("../bills/bills-model.js");
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  const bills = [
-    {
-      id: 1,
-      name: "Car",
-      amount: 650,
-      date: "2020-04-05",
-      repeat: true,
-      paid: false,
-      message: "this is a message",
-    },
-    {
-      id: 2,
-      name: "Rent",
-      amount: 1400,
-      date: "2020-04-13",
-      repeat: true,
-      paid: false,
-      message: "this is a message",
-    },
-    {
-      id: 3,
-      name: "Internet",
-      amount: 170,
-      date: "2020-04-18",
-      repeat: true,
-      paid: false,
-      message: "this is a message",
-    },
-  ];
+  Bills.find()
+    .then((bills) => {
+      res.status(200).json(bills);
+    })
+    .catch((err) => {
+      res.status(500).json({ errMessage: "Failed to get bills!" });
+    });
+});
 
-  res.status(200).json(bills);
+// ------------------------> GET DATA FROM DB
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  Bills.findById(id)
+    .then((bills) => {
+      if (bills) {
+        res.status(200).json(bills);
+      } else {
+        res
+          .status(404)
+          .json({ errMessage: `Could not find bill with id:${id}` });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ errMessage: "Failed to get bills!" });
+    });
+});
+
+// ------------------------> ADD DATA TO DB
+router.post("/", (req, res) => {
+  const { bill } = req.body;
+
+  Bills.add(bill)
+    .then((ids) => {
+      res.status(201).json({ created: ids[0] });
+    })
+    .catch((err) => {
+      res.status(500).json({ errMessage: "Failed to create new bill!" });
+    });
+});
+
+// ------------------------> UPDATE DATA IN DB
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  Bills.update(id, changes)
+    .then((bill) => {
+      if (bill) {
+        res.status(200).json(bill);
+      } else {
+        res
+          .status(404)
+          .json({ errMessage: `Could not find bill with given id:${id}` });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ errMessage: "Failed to update bills!" });
+    });
 });
 
 module.exports = router;
